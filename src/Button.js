24141e0b7dbc1c9a0ebcc1fef95a4ff6d9d99e12
won junho <wnsgh1993@gmail.com>
2022-07-07 22:28:47 +0900

    add styled component and do some css work

---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+	color : black;
+	background-color: tomato;
+`;
+
+function Button({text}) {
+	return <StyledButton>{text}</StyledButton>
+}
+
+Button.propTypes = {
+	text : PropTypes.string.isRequired,
+};
+
+export default Button;
